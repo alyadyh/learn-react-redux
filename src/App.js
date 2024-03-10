8f5d96c6b7e0bdb260bcbe57cdf56814b9841ae6
Alya Dhiya <alya.mardhiyyah@binus.ac.id>
2024-03-11 00:09:15 +0700

    add routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
-import Header from './containers/Header';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ProductListing from "./containers/ProductListing";
+import Header from "./containers/Header";
+import "./App.css";
+import ProductDetail from "./containers/ProductDetail";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" exact component={ProductDetail} />
+          <Route>404 Not Found!</Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
