a98fed829bb55cf0662e44edc6b03630149363a7
Alya Dhiya <alya.mardhiyyah@binus.ac.id>
2024-03-11 00:15:24 +0700

    use selector to access state

--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { useSelector } from "react-redux";
 
 const ProductComponent = () => {
-    return (
-        <div>
-            <h1>ProductComponent</h1>
+    const products = useSelector((state) => state.allProducts.products);
+    const renderList = products.map((product) => {
+      const { id, title } = product[0];
+      return (
+        <div className="four wide column">
+            <div className="ui link cards">
+              <div className="card">
+                <div className="image"></div>
+                <div className="content">
+                  <div className="header">{title}</div>
+                </div>
+              </div>
+            </div>
         </div>
-    )
-};
+      );
+    });
+    return <>{renderList}</>;
+  };
 
 export default ProductComponent;
