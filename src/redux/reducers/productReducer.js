4af2428e87f10e7cb682e4079971bf140ab98d53
Alya Dhiya <alya.mardhiyyah@binus.ac.id>
2024-03-11 00:45:50 +0700

    render product listing page

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -11,4 +11,16 @@ export const productsReducer = (state = initialState, { type, payload }) => {
       default:
         return state;
     }
-  };
+};
+
+export const selectedProductsReducer = (state = {}, { type, payload }) => {
+  console.log(type);
+  switch (type) {
+    case ActionTypes.SELECTED_PRODUCT:
+      return { ...state, ...payload };
+    case ActionTypes.REMOVE_SELECTED_PRODUCT:
+      return {};
+    default:
+      return state;
+  }
+};
