0a7f24dee6238fdabc464fe5c2da47058ddf510a
Alya Dhiya <alya.mardhiyyah@binus.ac.id>
2024-03-10 18:59:42 +0700

    add redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,10 @@ import { createStoreHook } from "redux";
 import reducers from './reducers/index';
 import { createStoreHook } from "react-redux";
 
-const store = createStoreHook(reducers, {});
+const store = createStoreHook(
+    reducers, 
+    {}, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
 export default store;
